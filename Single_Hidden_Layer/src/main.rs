de5a73265cb76f1c214242f03a33fa57e90a3360
Ted Pilcher <ted.pilcher@glide.co.uk>
2024-06-02 11:46:43 +0100

    starting forward propagation work

--- Single_Hidden_Layer/src/main.rs
+++ Single_Hidden_Layer/src/main.rs
@@ -4,12 +4,15 @@ use rand::{distributions::{Distribution, Uniform}, rngs::ThreadRng};
 
 struct NN {
 
+    num_input_nodes: i32,
+    num_output_nodes: i32,
     num_hidden_nodes: i32,
     weights_1: Vec<Vec<f32>>,
     weights_2: Vec<Vec<f32>>,
-    bias_1: Vec<f32>,
-    bias_2: Vec<f32>,
+    bias_1: Vec<Vec<f32>>,
+    bias_2: Vec<Vec<f32>>,
 }
+
 
 fn init_layer_weights(weights: &mut Vec<Vec<f32>>, step: &Uniform<f32>, rng: &mut ThreadRng, prev_layer_size: i32, curr_layer_size: i32){
 
@@ -26,12 +29,16 @@ fn init_layer_weights(weights: &mut Vec<Vec<f32>>, step: &Uniform<f32>, rng: &mu
     }
 }
 
-fn init_bias(bias: &mut Vec<f32>, num_nodes : i32){
-
+fn init_bias(bias: &mut Vec<Vec<f32>>, num_nodes : i32){
+    
+    let mut row: Vec<f32> = Vec::new();
     for _i in 0..num_nodes {
-
-        bias.push(0.0)
+        
+        row.push(0.0)
     }
+
+    bias.push(row)
+
 }
 
 fn build_model (my_num_hidden_nodes : i32) -> NN {
@@ -41,6 +48,8 @@ fn build_model (my_num_hidden_nodes : i32) -> NN {
 
     // instantiate model
     let mut model = NN {
+        num_input_nodes: INPUT_SIZE,
+        num_output_nodes: OUTPUT_SIZE,
         num_hidden_nodes: my_num_hidden_nodes,
         weights_1: Vec::new(),
         weights_2: Vec::new(),
@@ -64,13 +73,50 @@ fn build_model (my_num_hidden_nodes : i32) -> NN {
     return model;
 }
 
-fn forward_propagation (model : &mut NN) {
+fn dot_product(a : &mut Vec<Vec<f32>>, b : &mut Vec<Vec<f32>>, z : &mut Vec<Vec<f32>>){
+
+    let a_rows = a.len();
+    let a_cols = a[0].len();
+    let b_cols = b[0].len();
+
+    let mut result: Vec<Vec<f32>> = vec![vec![0.0; b_cols]; a_rows];
+
+    for i in 0..a_rows {
+
+        for j in 0..b_cols {
+
+            let mut sum: f32 = 0.0;
+            for k in 0..a_cols {
+
+                sum += a[i][k] * b[k][j];
+            }
+
+            z[i][j] = sum;
+        }
+    }
+}
+
+fn add_bias () {
+
+}
+
+fn forward_propagation (model : &mut NN, x : &mut Vec<f32>) {
 
     
+    // calculate z1, dot product of x and W1 then add b1 bias
+
+    // calculate a1
+
+
+    // calculate z2
+
+
 }
 
 
 fn main(){
+
+    // need to define dataset to train on
 
     let mut model = build_model(5);
 
